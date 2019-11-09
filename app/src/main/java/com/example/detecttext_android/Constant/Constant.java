fc5236b9e776fa034131537e2ad4ab80127b97ed
Volkan <sonmez.volkan92@gmail.com>
2019-11-09 20:23:57 +0300

    Dialog düzeltildi

--- app/src/main/java/com/example/detecttext_android/Constant/Constant.java
+++ app/src/main/java/com/example/detecttext_android/Constant/Constant.java
@@ -3,5 +3,5 @@ package com.example.detecttext_android.Constant;
 import java.util.ArrayList;
 
 public class Constant {
-    public static String[] SEARCHED_WORD = new String[] { "Gillette", "Menemen", "Pantene", "Kaşarlı"};
+    public static String[] SEARCHED_WORD = new String[] { "Gillette", "Orkid", "Menemenee", "Pantene", "Kaşarasdlı"};
 }
